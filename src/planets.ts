58684a54a8230f82522e8bd0e3540ecfcc92ecf8
Mahesh Markose J <mamarkos@microsoft.com>
2022-09-26 00:56:56 +0530

    Added scale to solar system | Updated default vals

--- src/planets.ts
+++ src/planets.ts
@@ -11,13 +11,11 @@ enum Radius {
     // Uranus = 25,
     // Neptune = 24,
     // Sun = 695
-
     // Radius values adjusted to create the planets faster
     Jupiter = 35,
     Saturn = 30,
     Uranus = 13,
     Neptune = 12,
-
     Sun = 50,
 }
 
@@ -42,7 +40,6 @@ enum DistanceFromSun {
     // Saturn = 356,
     // Uranus = 717,
     // Neptune = 1124,
-
     // Distance values adjusted to view them better
     Jupiter = 97,
     Saturn = 178,
@@ -53,12 +50,13 @@ enum DistanceFromSun {
 namespace solarsystem {
     //% blockId=solarsystem_planet_mercury block="mercury at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=2
     //% weight=99
-    //% radius.defl=5
     export function create_mercury(
         center: Position,
-        radius?: number = Radius.Mercury
+        radius: number = Radius.Mercury
     ): void {
+        if (radius <= 0) return;
         shapes.sphere(
             LIGHT_GRAY_CONCRETE,
             center,
@@ -69,63 +67,77 @@ namespace solarsystem {
 
     //% blockId=solarsystem_planet_venus block="venus at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
-    //% radius.defl=Radius.Venus
+    //% radius.defl=6
     //% weight=98
-    export function create_venus(center: Position, radius?: number = 1): void {
+    export function create_venus(
+        center: Position,
+        radius: number = Radius.Venus
+    ): void {
+        if (radius <= 0) return;
         shapes.sphere(YELLOW_CONCRETE, center, radius, ShapeOperation.Outline);
     }
 
     //% blockId=solarsystem_planet_earth block="earth at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=6
     //% weight=97
-    export function create_earth(center: Position, radius?: number): void {
+    export function create_earth(
+        center: Position,
+        radius: number = Radius.Earth
+    ): void {
+        if (radius <= 0) return;
         solarsystem.sphere(
             [GRASS, DIRT],
             center,
-            Radius.Earth,
+            radius,
             ShapeOperation.Outline
         );
     }
 
     //% blockId=solarsystem_planet_mars block="mars at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=3
     //% weight=96
-    export function create_mars(center: Position, radius?: number): void {
-        shapes.sphere(
-            RED_SANDSTONE,
-            center,
-            Radius.Mars,
-            ShapeOperation.Outline
-        );
+    export function create_mars(
+        center: Position,
+        radius: number = Radius.Mars
+    ): void {
+        if (radius <= 0) return;
+        shapes.sphere(RED_SANDSTONE, center, radius, ShapeOperation.Outline);
     }
 
     //% blockId=solarsystem_planet_jupiter block="jupiter at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=35
     //% weight=95
-    export function create_jupiter(center: Position, radius?: number): void {
+    export function create_jupiter(
+        center: Position,
+        radius: number = Radius.Jupiter
+    ): void {
+        if (radius <= 0) return;
         shapes.sphere(
             ORANGE_CONCRETE_POWDER,
             center,
-            Radius.Jupiter,
+            radius,
             ShapeOperation.Outline
         );
     }
 
     //% blockId=solarsystem_planet_saturn block="saturn at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=30
     //% weight=94
-    export function create_saturn(center: Position, radius?: number): void {
+    export function create_saturn(
+        center: Position,
+        radius: number = Radius.Saturn
+    ): void {
+        if (radius <= 0) return;
         center = center.toWorld();
-        shapes.sphere(
-            GOLD_BLOCK,
-            center,
-            Radius.Saturn,
-            ShapeOperation.Outline
-        );
+        shapes.sphere(GOLD_BLOCK, center, radius, ShapeOperation.Outline);
         shapes.circle(
             GOLD_BLOCK,
             center,
-            Radius.Saturn + 3,
+            radius + 3,
             Axis.Y,
             ShapeOperation.Outline
         );
@@ -133,36 +145,52 @@ namespace solarsystem {
 
     //% blockId=solarsystem_planet_uranus block="uranus at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=13
     //% weight=93
-    export function create_uranus(center: Position, radius?: number): void {
+    export function create_uranus(
+        center: Position,
+        radius: number = Radius.Uranus
+    ): void {
+        if (radius <= 0) return;
         shapes.sphere(
             LIGHT_BLUE_CONCRETE_POWDER,
             center,
-            Radius.Uranus,
+            radius,
             ShapeOperation.Outline
         );
     }
 
     //% blockId=solarsystem_planet_neptune block="neptune at center %center=minecraftCreatePosition || of radius %radius"
     //% group="Planets"
+    //% radius.defl=12
     //% weight=92
-    export function create_neptune(center: Position, radius?: number): void {
-        shapes.sphere(
-            BLUE_CONCRETE,
-            center,
-            Radius.Neptune,
-            ShapeOperation.Outline
-        );
+    export function create_neptune(
+        center: Position,
+        radius: number = Radius.Neptune
+    ): void {
+        if (radius <= 0) return;
+        shapes.sphere(BLUE_CONCRETE, center, radius, ShapeOperation.Outline);
     }
 
     //% blockId=solarsystem_sun block="sun at center %center=minecraftCreatePosition || of radius %radius"
-    export function create_sun(center: Position, radius?: number): void {
-        shapes.sphere(GLOWSTONE, center, Radius.Sun, ShapeOperation.Outline);
+    //% group="Stars"
+
+    export function create_sun(
+        center: Position,
+        radius: number = Radius.Sun
+    ): void {
+        if (radius <= 0) return;
+        shapes.sphere(GLOWSTONE, center, radius, ShapeOperation.Outline);
     }
 
-    //% blockId=solarsystem block="solar system at center %center=minecraftCreatePosition"
+    /**
+     * Creates a solar system at a deafult scale of 10.
+     * @param scale scale of radius of planets; eg: 1, 10; Multiplies the actual radius by (scale/10)
+     */
+    //% blockId=solarsystem block="solar system at center %center=minecraftCreatePosition || scale %scale"
     //% group="Solar System"
-    export function create_solar_system(center: Position): void {
+    //% scale.min=1 scale.max=10 scale.defl=10
+    export function create_solar_system(center: Position, scale: number): void {
         const xc = Math.round(center.getValue(Axis.X));
         const yc = Math.round(center.getValue(Axis.Y));
         const zc = Math.round(center.getValue(Axis.Z));
@@ -201,14 +229,14 @@ namespace solarsystem {
             pos(xc + DistanceFromSun.Neptune, yc, zc)
         );
 
-        create_sun(center);
-        create_mercury(mercuryCenter);
-        create_venus(venusCenter);
-        create_earth(earthCenter);
-        create_mars(marsCenter);
-        create_jupiter(jupiterCenter);
-        create_saturn(saturnCenter);
-        create_uranus(uranusCenter);
-        create_neptune(neptuneCenter);
+        create_sun(center, Radius.Sun * (scale / 10));
+        create_mercury(mercuryCenter, Radius.Mercury * (scale / 10));
+        create_venus(venusCenter, Radius.Venus * (scale / 10));
+        create_earth(earthCenter, Radius.Earth * (scale / 10));
+        create_mars(marsCenter, Radius.Mars * (scale / 10));
+        create_jupiter(jupiterCenter, Radius.Jupiter * (scale / 10));
+        create_saturn(saturnCenter, Radius.Saturn * (scale / 10));
+        create_uranus(uranusCenter, Radius.Uranus * (scale / 10));
+        create_neptune(neptuneCenter, Radius.Neptune * (scale / 10));
     }
 }
